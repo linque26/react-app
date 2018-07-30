546dee6a1ea9003bc662291c0e070276f8a1b4fa
@linque26 <linque26@gmail.com>
2018-07-30 13:49:45 -0400

    third commit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
+import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1>HELLO CANADA hey!!</h1>
+        <h1>HELLO CANADA Heyyy!!</h1>
       </div>
     );
   }
